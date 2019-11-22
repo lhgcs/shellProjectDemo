3ab6721da08b23d9170624a908a325673444ea7e
ubuntu16 <2640340412@qq.com>
2019-11-22 16:23:17 +0800

    test

--- clear.sh
+++ clear.sh
@@ -4,11 +4,18 @@
  # @Description: 清空
  # @Author: your name
  # @Date: 2019-10-26 14:53:13
- # @LastEditTime: 2019-10-26 14:53:33
- # @LastEditors: Please set LastEditors
+ # @LastEditTime: 2019-11-22 16:00:37
+ # @LastEditors: lhgcs
 ###
 
 sudo mv /var/lib/dpkg/info /var/lib/dpkg/info.old
 sudo mkdir /var/lib/dpkg/info
-sudo apt-get update
+
 sudo apt-get -f install
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo rm -rf /var/cache/apt/archives/*.deb
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-get clean
